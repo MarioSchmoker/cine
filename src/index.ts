8bd0ee50bdc550e916f228d5fc1de5f66077eb92
Mario Schmoker <103613451+MarioSchmoker@users.noreply.github.com>
2022-06-04 00:00:05 -0300

    practica

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,28 @@
-let cantidadAsientos : number = Number(prompt(`Ingrese la cantidad de Asientos: `));
-let asientos : number[] = new Array (cantidadAsientos);
-let indice : number = 0 ;
-let totalVacias : number = 0;
-let totalOcupadas : number = 0;
-let vacias : number = true //1
-let ocupadas : number = false; //0
+let cantidadAsientos: number = Number(
+  prompt(`Ingrese la cantidad de Asientos: `)
+);
+let asientos: number[] = new Array(cantidadAsientos);
+let indice: number = 0;
+let totalVacias: number = 0;
+let totalOcupadas: number = 0;
+let vacias: boolean = true; //1
+let ocupadas: boolean = false; //0
 
-function isEmptySeat(vacias: boolean = true , ocupadas: boolean = false ) : number {
-   return Math.floor(Math.random()*100);
+function isEmptySeat(
+  vacias: boolean = true,
+  ocupadas: boolean = false
+): number {
+  return Math.floor(Math.random() * 10);
 }
-  
-for (indice = 0; indice < cantidadAsientos ; indice++) {
-      
-      asientos[indice] = isEmptySeat(true,false)  
-  
-        if (true){
-            totalVacias += vacias
-        } else (false){
-             totalOcupadas += ocupadas
-          };
+
+for (indice = 0; indice < cantidadAsientos; indice++) {
+  asientos[indice] = isEmptySeat(true, false);
+
+  if (true) {
+    totalOcupadas += 1;
+  } else if (false) {
+    totalVacias += 1;
+  }
 }
+console.log("Butacas vacias : ", totalVacias);
+console.log("Butacas ocupadas : ", totalOcupadas);
